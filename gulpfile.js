f9c7fc180a48ce835db8c6e9be2166aafb92d9ed
Cesar <rodascesar03@yahoo.com>
2016-12-17 22:50:36 -0600

    added styles task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,2 @@
-var gulp = require('gulp');
-var webpack = require('gulp-webpack');
-
-gulp.task('default', function(){
-  return gulp.src('src/main.js')
-    .pipe(webpack( require('./webpack.config.js') ))
-    .pipe(gulp.dest('build/'));
-});
+require('./gulp/webpack');
+require('./gulp/styles');
